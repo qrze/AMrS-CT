228064d6f3235f57061d4fa9c5c51a15ee9f5125
pwwraisedd <143831425+qrze@users.noreply.github.com>
2026-04-13 17:04:15 +0100

    it works but its completely unbalance and some stuff got errors BUT IT PASSED THE PARSE

--- AMrS.js
+++ AMrS.js
@@ -10,7 +10,7 @@ var description =
     "x (position) chases E (equilibrium) via logistic growth, while E is driven by x raised to a power α. " +
     "S (stability) and D (stress) modulate growth, creating rich multi-regime behaviour.";
 var authors = "pwwraisedd, melon";
-var version = 10;
+var version = 9;
 requiresGameVersion("1.4.33");
 
 var logX = -1;
